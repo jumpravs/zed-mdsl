83634cb2a3131f8580ce8b81580b8078676e8685
jumpravs <58113606+jumpravs@users.noreply.github.com>
2024-11-26 00:38:08 +0200

    Initial work

---
+++ README.md
@@ -0,0 +1,11 @@
+## MDSL Language support for Zed Editor
+
+Title says it all.
+
+## Author
+
+jumpravs (github.com/jumpravs)
+
+## License
+
+MIT
---
+++ languages/mdsl/highlights.scm
@@ -0,0 +1,29 @@
+[
+  (root) @keyword
+  (sequence) @keyword
+  (selector) @keyword
+  (parallel) @keyword
+  (race) @keyword
+  (all) @keyword
+  (lotto) @keyword
+  (repeat) @keyword
+  (retry) @keyword
+  (flip) @keyword
+  (succeed) @keyword
+  (fail) @keyword
+  (action) @function
+  (condition) @function
+  (wait) @function.builtin
+  (branch) @function
+  (entry) @function.builtin
+  (exit) @function.builtin
+  (step) @function.builtin
+  (while) @conditional
+  (until) @conditional
+  (comment) @comment
+  (identifier) @variable
+  (number) @number
+  (string) @string
+  (boolean) @constant.builtin
+  "null" @constant.builtin
+]
